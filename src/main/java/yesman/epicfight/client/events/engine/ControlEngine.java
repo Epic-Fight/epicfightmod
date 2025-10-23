5515443463e5a6edba5f916e349dc25cc9d32163
Ellet <echo.ellet@gmail.com>
2025-10-23 13:22:25 +0300

    refactor: rename method name since it works differently than the others

--- src/main/java/yesman/epicfight/client/events/engine/ControlEngine.java
+++ src/main/java/yesman/epicfight/client/events/engine/ControlEngine.java
@@ -292,7 +292,7 @@ public class ControlEngine {
 		}
 
 		if (isSwitchOrDropBlocked()) {
-			consumeHotbarSlotKeyClicks();
+			disableHotbarSlotPresses();
 			consumeDropKeyClicks();
 		}
 	}
@@ -525,7 +525,7 @@ public class ControlEngine {
 	public void lockHotkeys() {
 		this.hotbarLocked = true;
 		this.lastHotbarLockedTime = this.player.tickCount;
-        consumeHotbarSlotKeyClicks();
+        disableHotbarSlotPresses();
 	}
 	
 	public void unlockHotkeys() {
@@ -746,7 +746,7 @@ public class ControlEngine {
     }
 
     /**
-     * Consumes hotbar slot key presses (keyboard only).
+     * Disables hotbar slot key presses (keyboard only).
      * <p>
      * This feature is strictly for keyboards and will not support controllers,
      * as controllers have limited buttons. Keyboard users can switch slots via
@@ -755,7 +755,7 @@ public class ControlEngine {
      *
      * @see ControlEngine#isHotbarCyclingDisabled
      */
-    private static void consumeHotbarSlotKeyClicks() {
+    private static void disableHotbarSlotPresses() {
         final KeyMapping[] hotbarSlots = Minecraft.getInstance().options.keyHotbarSlots;
         for (int i = 0; i < 9; ++i) {
             final KeyMapping hotbarSlot = hotbarSlots[i];

4f52b25a568cbf70fa2ecfd5226f662d4d65070b
Ellet <echo.ellet@gmail.com>
2025-11-23 18:48:32 +0300

    refactor: move internal DiscreteInputActionTrigger outside of the public API (yesman.epicfight.api.client.input -> yesman.epicfight.client.input)

--- src/main/java/yesman/epicfight/api/client/input/handlers/InputManager.java
+++ src/main/java/yesman/epicfight/api/client/input/handlers/InputManager.java
@@ -21,6 +21,7 @@ import yesman.epicfight.api.client.input.controller.ControllerBinding.InputType;
 import yesman.epicfight.api.client.input.controller.EpicFightControllerModProvider;
 import yesman.epicfight.api.client.input.controller.IEpicFightControllerMod;
 import yesman.epicfight.client.events.engine.ControlEngine;
+import yesman.epicfight.client.input.DiscreteInputActionTrigger;
 
 /**
  * High-level input API that abstracts direct interactions with {@link KeyMapping}

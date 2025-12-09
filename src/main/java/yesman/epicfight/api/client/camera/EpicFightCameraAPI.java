5a3213f7e6c0882331de83ecfde37f6c648c64bc
Yesssssman <yesman1058@gmail.com>
2025-12-09 12:47:41 +0900

    removed @deprecated from setNextLockOnTarget

--- src/main/java/yesman/epicfight/api/client/camera/EpicFightCameraAPI.java
+++ src/main/java/yesman/epicfight/api/client/camera/EpicFightCameraAPI.java
@@ -346,10 +346,6 @@ public final class EpicFightCameraAPI {
 		return ClientConfig.lockOnRange;
 	}
 	
-	/**
-	 * @Deprecated Use more parameters friendly version below
-	 */
-	@Deprecated(forRemoval = true)
 	public boolean setNextLockOnTarget(int direction) {
 		return this.setNextLockOnTarget(direction, false, true);
 	}

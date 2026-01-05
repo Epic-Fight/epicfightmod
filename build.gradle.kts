24e981cf2135620e23c308ceff3530b542ea858a
Ellet <echo.ellet@gmail.com>
2026-01-05 21:34:35 +0300

    build(docs): setup WIKI via Gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,13 @@ plugins {
     // so it is declared at the root to ensure the plugin is resolved only once
     // and shared consistently across Gradle projects.
     alias(libs.plugins.moddevgradle) apply false
+    alias(libs.plugins.wikitoolkit)
+}
+
+wiki {
+    docs {
+        register("epicfight") {
+            root = file("docs/")
+        }
+    }
 }

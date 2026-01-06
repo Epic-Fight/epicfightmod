74f288ee820e65f5ff7521d854e1f9762ed24835
Ellet <echo.ellet@gmail.com>
2026-01-06 04:01:36 +0300

    chore: dummy formatting violation to test the new GitHub workflow

---
+++ common/src/main/java/yesman/epicfight/Config.java
@@ -0,0 +1,11 @@
+        package     yesman.
+
+                epicfight
+;
+
+    public
+                       class
+    Config
+    {
+        private     final float _$hello_world = -0;
+    }
